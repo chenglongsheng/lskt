b7fe0f53a3bed47ac445795c714d8027e668970b
成龙生 <1536463948@qq.com>
2022-07-21 11:18:18 +0800

    修改状态码

--- lskt_parent/common/service_utils/src/main/java/person/cls/lskt/utils/result/Code.java
+++ lskt_parent/common/service_utils/src/main/java/person/cls/lskt/utils/result/Code.java
@@ -8,27 +8,27 @@ import lombok.Getter;
 @Getter
 public enum Code {
 
-    SUCCESS(200, "成功"),
-    FAIL(201, "失败"),
-    SERVICE_ERROR(2012, "服务异常"),
-    DATA_ERROR(204, "数据异常"),
-    ILLEGAL_REQUEST(205, "非法请求"),
-    REPEAT_SUBMIT(206, "重复提交"),
+    SUCCESS(20000, "成功"),
+    FAIL(20001, "失败"),
+    SERVICE_ERROR(20012, "服务异常"),
+    DATA_ERROR(20004, "数据异常"),
+    ILLEGAL_REQUEST(20005, "非法请求"),
+    REPEAT_SUBMIT(20006, "重复提交"),
 
-    LOGIN_AUTH(208, "未登陆"),
-    PERMISSION(209, "没有权限"),
+    LOGIN_AUTH(20008, "未登陆"),
+    PERMISSION(20009, "没有权限"),
 
-    PHONE_CODE_ERROR(211, "手机验证码错误"),
+    PHONE_CODE_ERROR(20011, "手机验证码错误"),
 
-    MTCLOUD_ERROR(210, "直播接口异常"),
+    MTCLOUD_ERROR(20010, "直播接口异常"),
 
-    COUPON_GET(220, "优惠券已经领取"),
-    COUPON_LIMIT_GET(221, "优惠券已发放完毕"),
+    COUPON_GET(20020, "优惠券已经领取"),
+    COUPON_LIMIT_GET(20021, "优惠券已发放完毕"),
 
     FILE_UPLOAD_ERROR(21004, "文件上传错误"),
     FILE_DELETE_ERROR(21005, "文件刪除错误"),
 
-    VOD_PLAY_ERROR(209, "请购买后观看");
+    VOD_PLAY_ERROR(20009, "请购买后观看");
 
     private final Integer code;
     private final String msg;
