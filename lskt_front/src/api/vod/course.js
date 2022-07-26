cec3e48a04aa041377115c42b50d0436531ce8b5
成龙生 <1536463948@qq.com>
2022-07-26 14:16:02 +0800

    课程分页列表

---
+++ lskt_front/src/api/vod/course.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+const api_name = '/vod/course'
+
+export default {
+    getPageList(current, limit, searchObj) {
+        return request({
+            url: `${api_name}/page/${current}/${limit}`,
+            method: `post`,
+            data: searchObj
+        })
+    },
+}
