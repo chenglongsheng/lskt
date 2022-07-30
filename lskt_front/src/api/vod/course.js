5c0c3eb928173529ebd16e275e780b4cbe74ac06
成龙生 <1536463948@qq.com>
2022-07-30 16:25:59 +0800

    发布课程接口

--- lskt_front/src/api/vod/course.js
+++ lskt_front/src/api/vod/course.js
@@ -23,17 +23,31 @@ export default {
             data: courseInfo
         })
     },
-    getCourseInfoById(id){
+    getCourseInfoById(id) {
         return request({
             url: `${api_name}/get/${id}`,
             method: `get`
         })
     },
-    updateCourseInfoById(courseInfo){
+    updateCourseInfoById(courseInfo) {
         return request({
             url: `${api_name}/update`,
             method: `post`,
             data: courseInfo
         })
-    }
+    },
+    //获取发布课程信息  
+    getCoursePublishById(id) {
+        return request({
+            url: `${api_name}/getCoursePublishVo/${id}`,
+            method: 'get'
+        })
+    },
+    //发布课程  
+    publishCourseById(id) {
+        return request({
+            url: `${api_name}/publishCourseById/${id}`,
+            method: 'put'
+        })
+    },
 }
