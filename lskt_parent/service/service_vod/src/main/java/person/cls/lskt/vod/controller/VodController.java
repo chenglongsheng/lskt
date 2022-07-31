ba5b263071e9a2309720ec17ec0148c2552305fb
成龙生 <1536463948@qq.com>
2022-07-31 11:09:43 +0800

    服务端上传视频

--- lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/controller/VodController.java
+++ lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/controller/VodController.java
@@ -1,9 +1,16 @@
 package person.cls.lskt.vod.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import person.cls.lskt.utils.result.Result;
 import person.cls.lskt.vod.service.VodService;
+import person.cls.lskt.vod.util.ConstantPropertiesUtil;
+import person.cls.lskt.vod.util.Signature;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/vod")
@@ -12,5 +19,18 @@ public class VodController {
     @Autowired
     private VodService vodService;
 
+    @PostMapping("uploadVideo")
+    public Result<String> uploadVideo(@RequestParam("file") MultipartFile file) throws IOException {
+        InputStream is = file.getInputStream();
+        String originalFilename = file.getOriginalFilename();
+        String video = vodService.uploadVideo(is, originalFilename);
+        return Result.ok(video);
+    }
+
+    @DeleteMapping("remove/{videoSourceId}")
+    public Result<String> removeVideo(@PathVariable String videoSourceId) {
+        vodService.removeVideo(videoSourceId);
+        return Result.ok(null);
+    }
 
 }
--- lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/service/VodService.java
+++ lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/service/VodService.java
@@ -1,4 +1,11 @@
 package person.cls.lskt.vod.service;
 
+import java.io.InputStream;
+
 public interface VodService {
+
+    String uploadVideo(InputStream is, String originalFilename);
+
+    void removeVideo(String videoSourceId);
+
 }
