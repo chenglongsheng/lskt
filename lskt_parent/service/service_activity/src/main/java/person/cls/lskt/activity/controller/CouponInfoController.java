025e1b84af407af7ecd07b8fccb523858347d7cf
成龙生 <1536463948@qq.com>
2022-07-31 17:17:34 +0800

    优惠券增删改查

--- lskt_parent/service/service_activity/src/main/java/person/cls/lskt/activity/controller/CouponInfoController.java
+++ lskt_parent/service/service_activity/src/main/java/person/cls/lskt/activity/controller/CouponInfoController.java
@@ -1,9 +1,15 @@
 package person.cls.lskt.activity.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import person.cls.lskt.activity.service.CouponInfoService;
+import person.cls.lskt.model.activity.CouponInfo;
+import person.cls.lskt.utils.result.Result;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +22,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/activity/couponInfo")
 public class CouponInfoController {
+
+    @Autowired
+    private CouponInfoService service;
+
+    @GetMapping("{page}/{limit}")
+    public Result<IPage<CouponInfo>> index(@PathVariable Long page, @PathVariable Long limit) {
+        IPage<CouponInfo> pageModel = service.page(new Page<>(page, limit));
+        return Result.ok(pageModel);
+    }
+
+    @GetMapping("get/{id}")
+    public Result<CouponInfo> get(@PathVariable Long id) {
+        CouponInfo couponInfo = service.getById(id);
+        return Result.ok(couponInfo);
+    }
+
+    @PutMapping("update")
+    public Result<CouponInfo> updateById(@RequestBody CouponInfo couponInfo) {
+        service.updateById(couponInfo);
+        return Result.ok(null);
+    }
+
+    @DeleteMapping("remove/{id}")
+    public Result<CouponInfo> remove(@PathVariable String id) {
+        service.removeById(id);
+        return Result.ok(null);
+    }
+
+    @DeleteMapping("batchRemove")
+    public Result<CouponInfo> batchRemove(@RequestBody List<String> idList) {
+        service.removeByIds(idList);
+        return Result.ok(null);
+    }
 
 }
 
