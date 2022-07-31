af16b509efac1d8843388214af9769b72c30b2e7
成龙生 <1536463948@qq.com>
2022-07-31 11:33:54 +0800

    注册服务

--- lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/ServiceVodApplication.java
+++ lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/ServiceVodApplication.java
@@ -2,9 +2,11 @@ package person.cls.lskt.vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("person.cls")
 public class ServiceVodApplication {
     public static void main(String[] args) {
