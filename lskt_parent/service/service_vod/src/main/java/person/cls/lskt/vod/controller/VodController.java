0a4e2de6ff98c7fbe0b44cdf65ed1e1e10c7ba3f
成龙生 <1536463948@qq.com>
2022-07-31 11:10:02 +0800

    客服端上传视频

--- lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/controller/VodController.java
+++ lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/controller/VodController.java
@@ -19,6 +19,26 @@ public class VodController {
     @Autowired
     private VodService vodService;
 
+    @GetMapping("sign")
+    public Result<String> sign() {
+        Signature sign = new Signature();
+        // 设置 App 的云 API 密钥
+        sign.setSecretId(ConstantPropertiesUtil.ACCESS_KEY_ID);
+        sign.setSecretKey(ConstantPropertiesUtil.ACCESS_KEY_SECRET);
+        sign.setCurrentTime(System.currentTimeMillis() / 1000);
+        sign.setRandom(new Random().nextInt(java.lang.Integer.MAX_VALUE));
+        sign.setSignValidDuration(3600 * 24 * 2); // 签名有效期：2天
+        try {
+            String signature = sign.getUploadSignature();
+            System.out.println("signature : " + signature);
+            return Result.ok(signature);
+        } catch (Exception e) {
+            System.out.print("获取签名失败");
+            e.printStackTrace();
+            return Result.fail("").message("获取签名失败");
+        }
+    }
+
     @PostMapping("uploadVideo")
     public Result<String> uploadVideo(@RequestParam("file") MultipartFile file) throws IOException {
         InputStream is = file.getInputStream();
