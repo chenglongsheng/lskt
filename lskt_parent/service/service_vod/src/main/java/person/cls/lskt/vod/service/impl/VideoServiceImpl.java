7714e597be7ddb470b6371139721be7871b81a39
成龙生 <1536463948@qq.com>
2022-07-31 11:26:31 +0800

    完善删除视频

--- lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/service/impl/VideoServiceImpl.java
+++ lskt_parent/service/service_vod/src/main/java/person/cls/lskt/vod/service/impl/VideoServiceImpl.java
@@ -1,11 +1,17 @@
 package person.cls.lskt.vod.service.impl;
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import person.cls.lskt.model.vod.Video;
 import person.cls.lskt.vod.mapper.VideoMapper;
 import person.cls.lskt.vod.service.VideoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import person.cls.lskt.vod.service.VodService;
+
+import javax.xml.ws.Action;
+import java.util.List;
 
 /**
  * <p>
@@ -18,10 +24,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class VideoServiceImpl extends ServiceImpl<VideoMapper, Video> implements VideoService {
 
+    @Autowired
+    private VodService vodService;
+
     @Override
     public void removeVideoByCourseId(Long id) {
-        super.remove(Wrappers.lambdaQuery(Video.class)
+        List<Video> list = super.list(Wrappers.lambdaQuery(Video.class)
                 .eq(id != null, Video::getCourseId, id));
+        for (Video video : list) {
+            String videoSourceId = video.getVideoSourceId();
+            if (!StringUtils.isEmpty(videoSourceId)) {
+                vodService.removeVideo(videoSourceId);
+            }
+        }
+        super.removeByIds(list);
     }
 
 }
